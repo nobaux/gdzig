667b950ad14cf706c27968b7dba7c37df73b517d
Tristan Pemble <tpemble@gmail.com>
2025-06-23 16:35:46 -0700

    style: rearrange code in order of argument index

--- binding_generator/Config.zig
+++ binding_generator/Config.zig
@@ -28,13 +28,14 @@ pub fn loadFromArgs(args: [][:0]u8) !Config {
     var vendor = try cwd.openDir(args[1], .{});
     defer vendor.close();
 
+    const extension_api = try vendor.openFile("extension_api.json", .{});
+    const gdextension_interface = try vendor.openFile("gdextension_interface.h", .{});
+
     try cwd.deleteTree(args[2]);
+    const output = try std.fs.cwd().makeOpenPath(args[2], .{});
 
     const arch = std.meta.stringToEnum(Config.Arch, args[3]) orelse std.debug.panic("Invalid architecture {s}, expected {any}", .{ args[3], std.meta.tags(Config.Arch) });
     const precision = std.meta.stringToEnum(Config.Precision, args[4]) orelse std.debug.panic("Invalid precision {s}, expected {any}", .{ args[3], std.meta.tags(Config.Precision) });
-    const extension_api = try vendor.openFile("extension_api.json", .{});
-    const gdextension_interface = try vendor.openFile("gdextension_interface.h", .{});
-    const output = try std.fs.cwd().makeOpenPath(args[2], .{});
     const verbosity = std.meta.stringToEnum(Config.Verbosity, args[5]) orelse .quiet;
 
     return .{
