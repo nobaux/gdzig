e163f20e8a661b24d471489f002c33edef52a7c8
Tristan Pemble <tpemble@gmail.com>
2025-07-15 11:34:53 -0700

    feature(#19): add coverage of Image interface functions

---
+++ gdzig_bindings/class/Image.mixin.zig
@@ -0,0 +1,24 @@
+/// Returns a mutable slice of the internal Image buffer.
+///
+/// **Since Godot 4.3**
+pub inline fn slice(self: *Image) []u8 {
+    const len = @as(usize, @intCast(self.getDataSize()));
+    const p = @as([*]u8, @ptrCast(raw.imagePtr(self.ptr())));
+    return p[0..len];
+}
+
+/// Returns a const slice of the internal Image buffer.
+///
+/// **Since Godot 4.3**
+pub inline fn constSlice(self: *const Image) []const u8 {
+    const len = @as(usize, @intCast(self.getDataSize()));
+    const p = @as([*]const u8, @ptrCast(raw.imagePtr(@constCast(self.constPtr()))));
+    return p[0..len];
+}
+
+// @mixin stop
+
+const raw: *Interface = &@import("../gdzig_bindings.zig").raw;
+
+const Interface = @import("../Interface.zig");
+const Image = @import("./image.zig").Image;
