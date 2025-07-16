80544391e60841236b6929047e85e80f1ea66b10
Tristan Pemble <tpemble@gmail.com>
2025-07-15 18:59:48 -0700

    fix: bugs

--- gdzig/builtin/Callable.mixin.zig
+++ gdzig/builtin/Callable.mixin.zig
@@ -1,16 +1,16 @@
-pub fn fromClosure(p_instance: anytype, p_function_ptr: anytype) Callable {
+pub fn fromClosure(p_instance: anytype, comptime p_function_ptr: anytype) Callable {
     // find the method on `p_instance` by pointer
     const T = comptime std.meta.Child(@TypeOf(p_instance));
     const decls = comptime std.meta.declarations(T);
 
-    var method_name: ?[:0]const u8 = null;
+    comptime var method_name: ?[:0]const u8 = null;
 
     inline for (decls) |decl| {
         const field = @field(T, decl.name);
         const p_func_ptr: *const anyopaque = @ptrCast(p_function_ptr);
         const decl_func_ptr: *const anyopaque = @ptrCast(&field);
 
-        if (p_func_ptr == decl_func_ptr) {
+        if (comptime p_func_ptr == decl_func_ptr) {
             method_name = decl.name;
             break;
         }
@@ -20,16 +20,17 @@ pub fn fromClosure(p_instance: anytype, p_function_ptr: anytype) Callable {
         std.debug.panic("Func pointer is not a method of the instance", .{});
     }
 
-    var method_string_name: StringName = .fromLatin1(method_name.?);
+    var method_string_name: StringName = .fromComptimeLatin1(method_name.?);
     defer method_string_name.deinit();
 
     return .initObjectMethod(oopz.upcast(*Object, p_instance), method_string_name);
 }
 // @mixin stop
 
+const std = @import("std");
+
+const oopz = @import("oopz");
+
+const Object = @import("../class/object.zig").Object;
 const Callable = @import("callable.zig").Callable;
 const StringName = @import("string_name.zig").StringName;
-const Object = @import("../class/object.zig").Object;
-
-const std = @import("std");
-const oopz = @import("oopz");
